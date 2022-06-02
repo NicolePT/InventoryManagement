610d231210a32dc989bdb701020e3281a434af3b
price <nicole_14_2015@hotmail.com>
2022-06-01 20:55:38 -0500

    Added Inventory Services

---
+++ src/app/components/services/item.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class ItemService {
+	private basePath = 'https://localhost:7052/api/inventory/';
+
+	constructor(private http: HttpClient) { }
+
+	getItems(): Observable<any> {
+		return this.http.get(this.basePath);
+	}
+
+	deleteItemByCode(code: string): Observable<any> {
+		return this.http.delete(this.basePath + code)
+	}
+
+	createItem(item: any): Observable<any> {
+		return this.http.post(this.basePath, item);
+	}
+
+	updateItem(id: number, item: any): Observable<any> {
+		return this.http.put(this.basePath + id, item);
+	}
+}
